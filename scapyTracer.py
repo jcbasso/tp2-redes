62434adc6f52e1ef4e43cf14f393ddc14c308882
Juan Cruz Basso <juan.basso@mercadolibre.com>
2017-05-21 22:57:50 -0300

    Arreglo asquerosidades de ema

--- scapyTracer.py
+++ scapyTracer.py
@@ -9,10 +9,10 @@ def customTraceroute(host):
     ttl = 1
     finishedTrace = False
     while not finishedTrace :
-        replyInfo = traceReplyTohost(host, ttl)
-        print replyInfo['src'] + ' with RTT -> ' + replyInfo['rtt']
+        src,tipo,rtt = traceReplyTohost(host, ttl)
+        print src + ' with RTT -> ' + rtt
         ttl += 1
-        finishedTrace = not replyInfo['type'] == timeExceededType
+        finishedTrace = not tipo == timeExceededType
 
 def traceReplyTohost(host, ttl):
     pkt = IP(dst=host, ttl=ttl) / ICMP()
@@ -20,7 +20,7 @@ def traceReplyTohost(host, ttl):
     start = time.time()
     reply = sr1(pkt, verbose=0)
     rtt = time.time() - start
-    return {'src': reply.src, 'type': reply.type, 'rtt': str(rtt)}
+    return reply.src, reply.type, str(rtt)
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Trace packages')
