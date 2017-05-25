731bcd1d822d7c015adb5e9e0741df424c40d320
dtilve <tilve.dana@gmail.com>
2017-05-25 12:51:45 -0300

    Refactor para mandar varios paquetes IP en lugar de uno solo - OJO ESTO NO COMPILA <3

--- scapyTracer.py
+++ scapyTracer.py
@@ -4,28 +4,46 @@ import argparse
 import time
 
 timeExceededType = 11
+number_of_packages = 30
 
-def customTraceroute(host,tries=3):
-    traceroutes = []
-    for i in xrange(0,tries):
-        iTraceroute = []
-        ttl = 1
-        finishedTrace = False
-        while not finishedTrace :
-            src,tipo,rtt = traceReplyTohost(host, ttl)
-            iTraceroute.append( src + ' with RTT -> ' + rtt )
-            ttl += 1
-            finishedTrace = not tipo == timeExceededType
-        traceroutes.insert(i,iTraceroute)
-    return traceroutes
+def trace(host):
+	ttl_to_routes = customTraceroute(host, number_of_packages)
 
-def traceReplyTohost(host, ttl):
-    pkt = IP(dst=host, ttl=ttl) / ICMP()
-    # Send the packet and get a reply
-    start = time.time()
-    reply = sr1(pkt, verbose=0)
-    rtt = time.time() - start
-    return reply.src, reply.type, str(rtt)
+	for ttl in ttl_to_routes:
+		# sacar los promedios descartando outliers y requests que nunca volvieron
+
+	# imprimir los promedios
+
+def customTraceroute(host,tries):
+	ttl_to_routes = dict()
+    ttl = 1
+    finishedTrace = False
+
+	while not finishedTrace :
+     	src_to_rtts = traceReplyTohost(host, tries, ttl)
+		ttl_to_routes[ttl] = src_to_rtts
+		# tipo = src_to_rtts[?].type ????????
+		finishedTrace = not tipo == timeExceededType
+		ttl += 1
+
+    return ttl_to_routes
+
+def traceReplyTohost(host, tries, ttl):
+	src_to_rtts = dict()
+
+	for i in tries:
+		pkt = IP(dst=host, ttl=ttl) / ICMP()
+	    # Send the packet and get a reply
+	    start = time.time()
+	    reply = sr1(pkt, verbose=0)
+	    rtt = time.time() - start
+
+		if not(reply.src in src_to_rtts):
+			src_to_rtts[reply.src] = [rtt]
+		else:
+			src_to_rtts[reply.src].append(rtt)
+
+    return src_to_rtts
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Trace packages')
