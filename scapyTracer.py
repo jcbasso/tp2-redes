a33e42c79d61f23381686f9edfe83d2ce77dcb14
dtilve <tilve.dana@gmail.com>
2017-05-27 21:58:28 -0300

    Traceroute funcionando :D!

--- scapyTracer.py
+++ scapyTracer.py
@@ -1,57 +1,65 @@
 #! /usr/bin/env python
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 import argparse
 import time
+import json
 
-timeExceededType = 11
-number_of_packages = 30
+TIME_EXCEEDED_TYPE = 11
+MAX_HOPS = 30
+NUMBER_OF_PACKAGES = 30
+TIMEOUT = 5
+REACHED_END_CONDITION = False
 
 def trace(host):
-	ttl_to_routes = customTraceroute(host, number_of_packages)
+	ttl_to_routes = custom_traceroute(host, NUMBER_OF_PACKAGES)
+	print(json.dumps(ttl_to_routes, indent = 4, separators =(',', ': ')))
 
-	for ttl in ttl_to_routes:
-		# sacar los promedios descartando outliers y requests que nunca volvieron
-
-	# imprimir los promedios
-
-def customTraceroute(host,tries):
+def custom_traceroute(host, number_of_packages):
 	ttl_to_routes = dict()
-    ttl = 1
-    finishedTrace = False
 
-	while not finishedTrace :
-     	src_to_rtts = traceReplyTohost(host, tries, ttl)
-		ttl_to_routes[ttl] = src_to_rtts
-		# tipo = src_to_rtts[?].type ????????
-		finishedTrace = not tipo == timeExceededType
-		ttl += 1
+	for i in range(1,MAX_HOPS+1):
+		if REACHED_END_CONDITION:
+			break
+		src_to_rtts = trace_reply_to_host(host, number_of_packages, i)
+		ttl_to_routes[i] = src_to_rtts
 
-    return ttl_to_routes
+	return ttl_to_routes
 
-def traceReplyTohost(host, tries, ttl):
+def trace_reply_to_host(host, number_of_packages, ttl):
 	src_to_rtts = dict()
 
-	for i in tries:
+	for i in range(0,number_of_packages):
+
 		pkt = IP(dst=host, ttl=ttl) / ICMP()
-	    # Send the packet and get a reply
-	    start = time.time()
-	    reply = sr1(pkt, verbose=0)
-	    rtt = time.time() - start
+		# Send the packet and get a reply
+		start = time.time()
+		print('about to send packet ' + str(i) +' to ' + str(host))
+		reply = sr1(pkt, verbose=0, timeout= TIMEOUT)
+		rtt = time.time() - start
 
-		if not(reply.src in src_to_rtts):
-			src_to_rtts[reply.src] = [rtt]
-		else:
-			src_to_rtts[reply.src].append(rtt)
+		if not (reply is None):
+			print('sent & received answer for packet ' + str(i) + ' from source ' + str(reply.src))
+			if not(reply.src in src_to_rtts):
+				src_to_rtts[reply.src] = [rtt]
+			else:
+				src_to_rtts[reply.src].append(rtt)
 
-    return src_to_rtts
+			if reply.type == TIME_EXCEEDED_TYPE:
+				print('reply type is time exceeded')
+			else:
+				print('reply type was ' + str(reply.type))
+				global REACHED_END_CONDITION
+				REACHED_END_CONDITION = True
+
+	return src_to_rtts
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Trace packages')
     parser.add_argument('host', help='a host to trace its route')
-    parser.add_argument('--TC', dest='tracer', default=customTraceroute, const=traceroute, nargs='?',
-            help='which traceroute implementation to use (default "traceroute")')
     return parser.parse_args()
 
 args = parseArgs()
 
-print args.tracer(args.host)
+trace(args.host)
